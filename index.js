330d3166f1653464a69a50e50f04b13bf22e5d02
Ernest <ernestwinata@gmail.com>
2023-04-20 06:54:03 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+// Add your cod
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({ name: name, email: email })
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id;
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message;
+    });
+}
+
+
+
+
+
